1dbdc0c5e9c62eec9a1bffdbb638ff1569682e07
Alexis Gomez <alexisgomezgnb@gmail.com>
2026-03-16 22:33:00 -0300

    avance con conversion en Distancia

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,11 @@
 // constante de overlay:
 const sobrecapa = document.querySelector(".overlay");
+let valorNumero = 0;
+let valorOption = "";
 
-// Constante para almacenar todos los (div/calculadora) dentro de overlay
+/* Const "all_div_calc" me devuelve un objeto "NodeList",
+de los Div con class "calc, y const "limpiacalc es una function que oculta todos los Div 
+con la clase "calc" del NodeList all_div_calc*/
 const all_div_calc = document.querySelectorAll(".calc");
 const limpiacalc = () => {
     all_div_calc.forEach((card_calc) => {
@@ -9,19 +13,30 @@ const limpiacalc = () => {
     })
 }
 
+// Function que primero al overlay le aplica display none y ejecuta la funcion limpiacalc
 const limpiaTodo = () => {
     sobrecapa.style.display = "none";
     limpiacalc();
+    const borrametadatainput = document.querySelectorAll(".AllInClear");
+    borrametadatainput.forEach((x) => {
+        x.value = "";
+    })
+    const borrametadataselect = document.querySelectorAll(".AllselectToIndex");
+    borrametadataselect.forEach((s) => {
+        s.selectedIndex = 0;
+    })
 }
-// constante para almacenar todos los botones para cerrar las Cards
+
+/* const btn_volver me devuelve un objeto NodeList con todos los botones con la clase "volver",
+y luego itero con un forEach a todos los items de ese NodeList para ejecutar en cada uno la
+funcion addEventListner que al escuchar "click" me ejecuta la function limpiatodo */
 const btn_volver = document.querySelectorAll(".volver");
-btn_volver.forEach((X) => {
+    btn_volver.forEach((X) => {
     X.addEventListener("click" , limpiaTodo);
 })
 
-
-
-//constantes de Div Distancia
+//constantes para almacenar los elementos con ID de Div:
+// constante de Div Distancia
 const botonActivaDist = document.getElementById("btn-dist");
 const cardMostrarDist = document.getElementById("distancia-mag");
 
@@ -47,11 +62,38 @@ const botonActivaElect = document.getElementById("btn-elect");
 const cardMostrarElect = document.getElementById("electr-mag");
 
 
-// Funciones para cada card:
+// Aca comienza las conversiones de unidades:
+// Conversion de Distancia:
+
+const distValueDom = document.getElementById("dist1"); //DOM value input number
+const distOptDom = document.getElementById("dist2"); //DOM value Select Option
+const converterDist = ()=>{
+    valorNumero = parseFloat(distValueDom.value);
+    valorOption = distOptDom.value;
+    if(valorOption === "" || isNaN(valorNumero)){
+        document.getElementById("kmout").innerText = "Ingrese datos";
+        document.getElementById("millaout").innerText = "Ingrese datos";
+        return;
+    } else if(valorOption === "kilo"){
+        document.getElementById("kmout").innerText = valorNumero;
+        document.getElementById("millaout").innerText = (valorNumero / 1.609).toFixed(3);
+    } else {
+        document.getElementById("kmout").innerText = (valorNumero * 1.609).toFixed(3);
+        document.getElementById("millaout").innerText = valorNumero;
+    }
+}
+distValueDom.addEventListener("input", converterDist);
+distOptDom.addEventListener("change", converterDist);
+
+// Conversion de Presion:
+
+//**************************************************************************** */
+// Funciones para (Mostrar) cada card de magnitudes al apretar el Boton en la card:
 botonActivaDist.addEventListener("click", () => {
     limpiaTodo();
     sobrecapa.style.display = "flex";
     cardMostrarDist.style.display = "flex";
+    converterDist();
 })
 
 botonActivaPress.addEventListener("click", () => {
